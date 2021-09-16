e83e27d7cdfd7f98acbba7b2a40a401543d8e17e
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-09-16 16:20:46 -0400

    cleaned up the code and added a few scripts

--- URLShorteningService/index.js
+++ URLShorteningService/index.js
@@ -15,23 +15,31 @@ app.get('/', (req, res, next) => {
 app.post('/shorten', (req, res, next) => {
     try {
         const body = req.body;
+        const url = body['url'];
         let hash = "";
-        if (body['hash'] == undefined) {
-            const existingHashes = Array.from(shortToLong.keys());
-            do {
-                hash = crypto.createHash('sha256').update(body['url']).digest('base64').substring(0, 7);
-            } while (existingHashes.includes(hash));
-        } else {
-            hash = body['hash'];
+        if (longToShort.has(url)) {
+            hash = longToShort.get(url);
         }
-        longToShort.set(body["url"], hash);
-        shortToLong.set(hash, body["url"]);
+        else {
+            if (body['hash'] == undefined) {
+            const existingHashes = new Set(shortToLong.keys());
+            do {
+                const consumer = body['url'] + Date.now();
+                hash = crypto.createHash('sha256').update(consumer).digest('base64').substring(0, 7);
+            } while (existingHashes.has(hash));
+            } else {
+                hash = body['hash'];
+            }
+            longToShort.set(body["url"], hash);
+            shortToLong.set(hash, body["url"]);
+        }
         return res.status(200).json({"msg": "success", "data": hash});
     } catch (err) {
+        console.log(err);
         return res.status(500).json({"msg": err, "data": null});
     }
 });
 
-app.listen(5000, () => {
-
+app.listen(5000, '0.0.0.0', () => {
+    console.log('running');
 })
