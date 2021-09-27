3d8ca2436a2a1ff70d45ac194a7836414223cd5a
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-09-26 23:27:39 -0400

    built a microservice to get posts within a certain radius using .NET Entity Framework

---
+++ Project/PostsService/PostsService/Controllers/PostsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PostsService.Models;
+using PostsService.Services;
+
+namespace PostsService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        // POST: api/Posts/PostsWithinMiles
+        [Route("PostsWithinMiles")]
+        [HttpPost()]
+        public IActionResult PostsWithinMiles([FromBody] PostRequest postRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(postRequest);
+            }
+            var result = PostService.GetPosts(postRequest.Miles, postRequest.Latitude, postRequest.Longitude);
+            return result.Code == 200 ? Ok(result) : StatusCode(500, result);
+        }
+
+        // POST api/posts/Create
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult Post([FromBody] Post post)
+        {
+            var result = PostService.CreatePost(post);
+            return result.Code == 200 ? Ok(result) : StatusCode(500, result);
+        }
+
+        // PUT api/posts/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/posts/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Project/PostsService/PostsService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace PostsService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Util.GenerateDatabaseConnectionString());
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args) 
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:5000");
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
