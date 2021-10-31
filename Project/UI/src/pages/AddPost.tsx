54a3862b5e551e7ac0a32dd765c16000562e18a9
Omri-Shavit <Shavit.omri@gmail.com>
2021-10-31 17:58:49 -0400

    plumbing for validation

--- Project/UI/src/pages/AddPost.tsx
+++ Project/UI/src/pages/AddPost.tsx
@@ -6,29 +6,53 @@ import {
     IonCardHeader,
     IonCardTitle,
     IonCardContent,
-    IonText
-    } 
+    IonText,
+    IonFooter,
+    IonToolbar,
+    IonButton
+    }
     from '@ionic/react';
 import "./AddPost.css";
-import React, { useState } from 'react';
+import React, { FormEventHandler, useState } from 'react';
 
-class AddPost extends React.Component<any, any> {
+interface AddPostState {
+    isMessageValid : boolean;
+    isTitleValid : boolean;
+}
+
+class AddPost extends React.Component<any, AddPostState> {
+    constructor (props : any) {
+        super(props);
+        this.state = {
+            isMessageValid : false,
+            isTitleValid : false
+        };
+    }
+    titleHandler (event : FormEventHandler<HTMLIonInputElement>) {
+        console.log(event);
+    }
     render() {
+        const isFormValid = this.state.isMessageValid && this.state.isTitleValid;
         return (
             <IonContent>
                 <IonText color="dark" className={"centered"}>
                     <h2>Add Post</h2>
                 </IonText>
                 <IonCard>
-                <IonCardHeader>
-                    <IonCardTitle>
-                        <IonInput placeholder="post title"></IonInput>
-                    </IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                    <IonTextarea placeholder="Your message here">
-                    </IonTextarea>
-                </IonCardContent>
+                    <IonCardHeader>
+                        <IonCardTitle>
+                            <IonInput onChange={this.titleHandler} placeholder="Post Title"></IonInput>
+                        </IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        <IonTextarea placeholder="Your Description Here">
+                        </IonTextarea>
+                    </IonCardContent>
+                    <IonFooter>
+                        <IonToolbar>
+                            <IonButton color={"secondary"} disabled={!isFormValid} expand={"block"} fill={"solid"}>Add Post</IonButton>
+                        </IonToolbar>
+                    </IonFooter>
                 </IonCard>
             </IonContent>
         );
