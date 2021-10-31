e0f0335d0608f74323b876b05ba322dcb49e2408
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-10-31 17:19:09 -0400

    more work on the addPost page

--- Project/UI/src/pages/AddPost.tsx
+++ Project/UI/src/pages/AddPost.tsx
@@ -2,19 +2,34 @@ import {
     IonTextarea,
     IonContent,
     IonInput,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonText
     } 
     from '@ionic/react';
-
-    import { thumbsUp} from 'ionicons/icons';
+import "./AddPost.css";
 import React, { useState } from 'react';
 
 class AddPost extends React.Component<any, any> {
     render() {
         return (
             <IonContent>
-                <IonInput placeholder="post title"></IonInput>
-                <IonTextarea placeholder="Your message here">
-                </IonTextarea>
+                <IonText color="dark" className={"centered"}>
+                    <h2>Add Post</h2>
+                </IonText>
+                <IonCard>
+                <IonCardHeader>
+                    <IonCardTitle>
+                        <IonInput placeholder="post title"></IonInput>
+                    </IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                    <IonTextarea placeholder="Your message here">
+                    </IonTextarea>
+                </IonCardContent>
+                </IonCard>
             </IonContent>
         );
     }
